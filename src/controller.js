484528934c81b469b4f4df5af6c5ddeefa613dc2
Sara Cook <saracook087@gmail.com>
2022-03-31 16:25:27 +0100

    added sailing

--- src/controller.js
+++ src/controller.js
@@ -1,19 +1,78 @@
 (function exportController() {
-  function Controller () {
+  function Controller(ship) {
+    this.ship = ship;
     this.initialiseSea();
+
+    document.querySelector("#sailButton").addEventListener("click", () => this.setSail());
   }
-  
-  Controller.prototype.initialiseSea = function initialiseSea() {
-    const backgrounds = [
-      "./images/water0.png",
-      "./images/water1.png"
-    ]
-    let backgroundIndex = 0;
-    window.setInterval(() => {
-      document.querySelector("#viewport").style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
-      backgroundIndex += 1;
-    }, 1000)
-  
+
+  Controller.prototype = {
+    initialiseSea() {
+      const backgrounds = [
+        "./images/water0.png",
+        "./images/water1.png"
+      ]
+      let backgroundIndex = 0;
+      window.setInterval(() => {
+        document.querySelector("#viewport").style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+        backgroundIndex += 1;
+      }, 1000)
+
+    },
+
+    renderPorts(ports) {
+      const portsElement = document.querySelector("#ports");
+      portsElement.style.width = "0px";
+
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement("div");
+        newPortElement.className = "port";
+
+        newPortElement.dataset.portName = port.name;
+        newPortElement.dataset.portIndex = index;
+
+        portsElement.appendChild(newPortElement);
+
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+      });
+    },
+
+    renderShip() {
+      const ship = this.ship;
+
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const portElement = document.querySelector(`[data-port-index = "${shipPortIndex}"]`);
+
+      const shipElement = document.querySelector("#ship");
+      shipElement.style.top = `${portElement.offsetTop + 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+    },
+
+    setSail() {
+      const ship = this.ship;
+
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort); 
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index = "${nextPortIndex}"]`);
+
+      if(!nextPortElement) {
+        return alert("End of the line!");
+      }
+
+      const shipElement = document.querySelector("#ship");
+
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.stye.left, 10);
+        if(shipLeft === (nextPortElement.offsetLeft - 32)) {
+          ship.setSail();
+          ship.dock();
+          clearInterval(sailInterval);
+        }
+
+        shipElement.stye.left = `${shipLeft + 1}px`;
+      }, 20)
+    }
   }
 
   if(typeof module !== "undefined" && module.exports) {
